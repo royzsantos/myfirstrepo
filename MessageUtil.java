07e82be903a721874237e8e986d799f6ce13e126
Roy Santos <santosrz@ph.ibm.com>
2019-06-18 22:33:23 +0800

    Add MessageUtil, TestJUnit, TestRunner

---
+++ MessageUtil.java
@@ -0,0 +1,13 @@
+public class MessageUtil {
+	
+	public String message;
+	
+	public MessageUtil(String message){
+		this.message = message;
+	}
+	
+	public String printMessage(){
+		System.out.println(message);
+		return message;
+	}
+}
